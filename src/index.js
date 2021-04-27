c51e31f1a0662d9989823cc5ec0b32050a12610e
Wilson-Patyal <patyalwilson@gmail.com>
2021-04-28 01:14:27 +0530

    Deploy

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/burgerBuilder/">
             <App />
         </BrowserRouter>
     </Provider>
