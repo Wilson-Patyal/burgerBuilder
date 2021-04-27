b0d28079d1af37ee4fbd97e33e51d43396f24b6d
Wilson-Patyal <patyalwilson@gmail.com>
2021-04-28 01:29:49 +0530

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -27,9 +27,9 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter basename="/burgerBuilder/">
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
 
